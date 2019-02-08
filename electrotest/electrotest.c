e1ec5337d4c2642b9f7df76c85e8e17572b3d839
David <sandqst@kth.se>
2019-02-08 07:59:07 +0100

    Calculate the E12-replacment resistanse

--- electrotest/electrotest.c
+++ electrotest/electrotest.c
@@ -101,10 +101,6 @@ void print_result(float res, float pow, float *res_arr, int res_arr_size) {
     printf("Effekt: %.2f W\n", pow);
 
     printf("Ersättningsresistanser i E12-serien kopplade i serie: ");
-    if (res_arr_size != 0) {
-        printf("0\n");
-        return;
-    }
     for (int i = 0; i < res_arr_size; ++i) {
         printf("%.1f%s", res_arr[i], i != res_arr_size - 1 ? ", " : "\n");
     }
